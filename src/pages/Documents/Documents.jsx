8a07a4109e07ebcfbb4c8b1d8018993de7c305c1
Bryan Xol <xolbryan@gmail.com>
2026-06-30 01:23:55 -0600

    fix(ui): grid breakpoint lg=6 for full-width forms on 2k (#7)

--- src/pages/Documents/Documents.jsx
+++ src/pages/Documents/Documents.jsx
@@ -148,7 +148,7 @@ export default function Documents() {
         <Box component="form" onSubmit={formik.handleSubmit}>
           <DialogContent>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={12} lg={6}>
                 <Autocomplete
                   options={employees}
                   getOptionLabel={(opt) => `${opt.fullName} (${opt.code})`}
@@ -161,13 +161,13 @@ export default function Documents() {
                   )}
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={12} lg={6}>
                 <TextField fullWidth label="Nombre del Documento" name="name" value={formik.values.name}
                   onChange={formik.handleChange} onBlur={formik.handleBlur}
                   error={formik.touched.name && Boolean(formik.errors.name)}
                   helperText={formik.touched.name && formik.errors.name} required />
               </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={12} lg={6}>
                 <TextField fullWidth label="Tipo" name="type" value={formik.values.type}
                   onChange={formik.handleChange} onBlur={formik.handleBlur} select required
                   error={formik.touched.type && Boolean(formik.errors.type)}
@@ -175,13 +175,13 @@ export default function Documents() {
                   {documentTypes.map((t) => <MenuItem key={t} value={t}>{t}</MenuItem>)}
                 </TextField>
               </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={12} lg={6}>
                 <TextField fullWidth label="URL del Archivo" name="fileUrl" value={formik.values.fileUrl}
                   onChange={formik.handleChange} onBlur={formik.handleBlur}
                   error={formik.touched.fileUrl && Boolean(formik.errors.fileUrl)}
                   helperText={formik.touched.fileUrl && formik.errors.fileUrl} />
               </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={12} lg={6}>
                 <TextField fullWidth label="Fecha de Vencimiento" name="expiresAt" type="date" value={formik.values.expiresAt}
                   onChange={formik.handleChange} onBlur={formik.handleBlur}
                   error={formik.touched.expiresAt && Boolean(formik.errors.expiresAt)}
